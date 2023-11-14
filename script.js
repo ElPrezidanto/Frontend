e92f77b8a7812f61ff41afaf6888c0bdf5c376d4
Enriki <enriki2004@mail.ru>
2023-11-14 23:34:07 +0300

    new

--- script.js
+++ script.js
@@ -1,37 +1,111 @@
-function promptUser() {
-
-    var userInput = prompt("Желаете пройти регистрацию на сайте? (Да/Нет)").toLowerCase();
-    if (userInput === "да") {
-        alert("Круто!");
-        registration();
-    } else {
-        alert("Попробуйте ещё раз.");
+function generateRandomString(length) {
+    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += str.charAt(Math.floor(Math.random() * str.length));
     }
+    return result;
 }
 
-// promptUser();
+function generateCaptcha() {
+    const captchaContainer = document.getElementById('captchaContainer');
+    const captchaText = generateRandomString(5);
 
-function registration() {
-    var login = prompt("Введите логин:", "").toLowerCase();
+    captchaContainer.innerHTML = `Введите:<strong>${captchaText}</strong>`;
+    captchaContainer.dataset.captchaType = 'letters';
+}
 
-    if (login === "админ") {
-        var password = prompt("Введите пароль:", "").toLowerCase();
+function generateNumberCaptcha() {
+    const captchaContainer = document.getElementById('captchaContainer');
+    const num1 = Math.floor(Math.random() * 25);
+    const num2 = Math.floor(Math.random() * 25);
 
-        if (password === "я главный") {
-            alert("Здравствуйте!");
-        } else if (password === null || password === "") {
-            alert("Отменено");
+    captchaContainer.innerHTML = `Введите сумму: ${num1} + ${num2}`;
+    captchaContainer.dataset.captchaType = 'numbers';
+}
+
+function checkCaptcha() {
+    const captchaInput = prompt('Введите значение капчи:');
+    const registerButton = document.getElementById('registerButton');
+    const captchaContainer = document.getElementById('captchaContainer');
+    const captchaType = captchaContainer.dataset.captchaType;
+
+    if (!isEmpty(captchaInput)) {
+        if (captchaType === 'letters') {
+            const captchaText = captchaContainer.querySelector('strong').innerText;
+            if (captchaInput === captchaText) {
+                alert('Правильно! Капча пройдена.');
+                registerButton.removeAttribute('disabled');
+            } else {
+                alert('Ошибка! Пожалуйста, попробуйте еще раз.');
+                generateNumberCaptcha();
+            }
         } else {
-            alert("Неверный пароль");
+            const [num1, num2] = captchaContainer.innerText.match(/\d+/g);
+            const expectedSum = parseInt(num1) + parseInt(num2);
+            if (parseInt(captchaInput) === expectedSum) {
+                alert('Правильно! Капча пройдена.');
+                registerButton.removeAttribute('disabled');
+            } else {
+                alert('Ошибка! Пожалуйста, попробуйте еще раз.');
+                generateCaptcha();
+            }
         }
-
-    } else if (login === null || login === "") {
-        alert("Отменено");
     } else {
-        alert("Я вас не знаю");
+        alert('Ошибка! Введите значение капчи.');
     }
 }
 
+function isEmpty(obj) {
+    return !obj.trim();
+}
+
+generateCaptcha();
+
+
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+
+    this.read = function () {
+        const userInput = prompt('Введите число:');
+        const parsedInput = parseFloat(userInput);
+
+        if (!isNaN(parsedInput)) {
+            this.value += parsedInput;
+        } else {
+            alert('Ошибка! Введите корректное число.');
+        }
+    };
+}
+
+const accumulator = new Accumulator(0);
+
+function addToCart() {
+    accumulator.read();
+    updateCartValue();
+}
+
+function updateCartValue() {
+    const cartValueContainer = document.getElementById('cartValueContainer');
+    cartValueContainer.innerHTML = `Сумма в корзине: ${accumulator.value}`;
+}
+
+
+
+
+function truncate(element, maxlength) {
+    const text = element.innerText;
+    element.innerText = text.length > maxlength ? text.slice(0, maxlength) + '…' : text;
+}
+
+// Вызываем функцию truncate для каждого заголовка h13
+truncate(document.getElementById('card1Title'), 15);
+truncate(document.getElementById('card2Title'), 15);
+truncate(document.getElementById('card3Title'), 15);
+
+
+/*
 const likeButton = document.getElementById('likeButton');
 const likeCount = document.querySelector('.like-count');
 const drawingContainer = document.querySelector('.drawing-container');
@@ -40,12 +114,39 @@ let count = 0;
 let isLiked = false;
 let isDrawing = false;
 
+var login = prompt("Введите логин:", "").toLowerCase();
+
+if (login === "админ") {
+    var password = prompt("Введите пароль:", "").toLowerCase();
+
+    if (password === "я главный") {
+        alert("Здравствуйте!");
+    } else if (password === null || password === "") {
+        alert("Отменено");
+    } else {
+        alert("Неверный пароль");
+    }
+
+} else if (login === null || login === "") {
+    alert("Отменено");
+} else {
+    alert("Я вас не знаю");
+}
+*/
+
+
 likeButton.addEventListener('click', function () {
     if (isLiked) {
         count--;
-        drawingContainer.innerHTML = '';
+        isDrawing = false;
+        drawingContainer.innerHTML = ''; // Очищаем контейнер при снятии лайка
+        likeButton.style.backgroundColor = 'white'
+        likeButton.style.color = 'black'
     } else {
+        isDrawing = true;
         count++;
+        likeButton.style.backgroundColor = 'black'
+        likeButton.style.color = 'white'
     }
 
     isLiked = !isLiked;
@@ -55,11 +156,7 @@ likeButton.addEventListener('click', function () {
 });
 
 document.addEventListener('mousedown', function () {
-    if (isLiked) {
-        isDrawing = true;
-    } else if (!isLiked) {
-        isDrawing = false;
-    }
+    isDrawing = true;
 });
 
 document.addEventListener('mouseup', function () {
@@ -72,8 +169,12 @@ document.addEventListener('mousemove', function (event) {
         dot.classList.add('dot');
         dot.style.left = event.pageX + 'px';
         dot.style.top = event.pageY + 'px';
+        dot.style.backgroundColor = 'white';
         drawingContainer.appendChild(dot);
     }
 });
 
-
+document.addEventListener('mouseleave', function () {
+    isDrawing = false;
+    drawingContainer.innerHTML = ''; // Очищаем контейнер при выходе за пределы окна
+});
